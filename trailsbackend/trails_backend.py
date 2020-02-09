48a92403607910d6d1f78f71c0097272869cbfaa
andb3 <andb3003@yahoo.com>
2020-02-08 23:14:22 -0500

    - single endpoint for inital load and updates

--- trailsbackend/trails_backend.py
+++ trailsbackend/trails_backend.py
@@ -15,48 +15,36 @@ app = Flask(__name__)
 ############## Regions ##############
 @app.route('/regions/')
 def regions_all():
-    return jsonify(region_repo.get_all_regions())
+    last_time = request.args.get('last') or 0
+    return jsonify(region_repo.get_all_region_updates(int(last_time)))
 
 @app.route('/regions/count')
 def regions_count():
     return jsonify(region_repo.get_regions_count())
-
-@app.route('/regions/new')
-def regions_updates():
-    last_time = request.args.get('last') or 0
-    return jsonify(region_repo.get_all_region_updates(int(last_time)))
 
 
 ############## Areas ##############
 
 @app.route('/areas/')
 def areas_all():
-    return jsonify(area_repo.get_all_areas())
+    last_time = request.args.get('last') or 0
+    return jsonify(area_repo.get_all_area_updates(int(last_time)))
 
 @app.route('/areas/count')
 def areas_count():
     return jsonify(area_repo.get_areas_count())
-
-@app.route('/areas/new')
-def areas_new():
-    last_time = request.args.get('last') or 0
-    return jsonify(area_repo.get_all_area_updates(int(last_time)))
 
 
 ############## Maps ##############
 
 @app.route('/maps/')
 def maps_all():
-    return jsonify(maps_repo.get_all_maps())
+    last_time = request.args.get('last') or 0
+    return jsonify(maps_repo.get_all_map_updates(int(last_time)))
 
 @app.route('/maps/count')
 def maps_count():
     return jsonify(maps_repo.get_maps_count())
-
-@app.route('/maps/new')
-def maps_updates():
-    last_time = request.args.get('last') or 0
-    return jsonify(maps_repo.get_all_map_updates(int(last_time)))
 
 
 ############## Test ##############
